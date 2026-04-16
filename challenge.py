b25d55f92ae311f4678c0f5c6e9f5179b9bab3e4
nandagopalanil01 <nandagopal.anil01@gmail.com>
2026-04-16 21:09:09 +0200

    loops challenge

---
+++ challenge.py
@@ -0,0 +1,7 @@
+#Print the 7-times table from 1 to 10 using a for loop
+for i in range(1, 11):
+    print(f"7 x {i} = {7*i}")
+
+#Print a left-alligned pyramid of stars with 6 rows using a for loop.
+for i in range(1, 7):
+    print("*" * i)
