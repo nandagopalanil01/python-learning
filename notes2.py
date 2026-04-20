5440e9fdefe192d97f447dab1e05a82a88fc2772
nandagopalanil01 <nandagopal.anil01@gmail.com>
2026-04-20 13:08:05 +0200

    while true

--- notes2.py
+++ notes2.py
@@ -1,14 +1,21 @@
-#useing loops to go through values and aggregate data like summing, counting, or averaging
-scores = [80, 50, 60, 75]
-total = 0
-for score in scores:
-    total += score
-    print("Current Total:", total)
-print("Final Total:", total)
+#while true 
+while True:
+    answer = input("Do you consent? (yes/no): ")
+    if answer == "yes":
+        break
+print("Thank You")
 
-#Using for loops to trasform data like cleaning data before processing
-
-files = [" Report.csv ", "DATA.csv ", " final.TXT"] #inconsistant casing & unnecessary spacing
-for file in files:
-    file = file.strip().lower().replace(".txt", ".csv")
-    print(f"processing {file}")
+#Challenge -1.allow up to 3 attempts
+#           2.if the user types yes print something
+#           3.Otherwise print"3 strikes you are out!"
+attempts = 0
+while attempts < 3:
+    answer = input("Do you agree the Messi is the GOAT? (yes/no)")
+    if answer == "yes":
+        print("Glad we are on the same page!")
+        break
+    attempts += 1
+else:
+    print("3 strikes and you are out")
+print("Thank you!")
+     
